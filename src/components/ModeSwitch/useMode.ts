1c1bdd5f9bf554c92f985abb12a58a8bb70d3827
Stef Verniers <91673619+Stef-Verniers@users.noreply.github.com>
2023-08-04 09:54:16 +0200

    Challenge 1 voltooid

--- src/components/ModeSwitch/useMode.ts
+++ src/components/ModeSwitch/useMode.ts
@@ -1,7 +1,30 @@
-import { useReducer } from "react"
+import { useSearchParams } from "react-router-dom"
 type Mode = "preview" | "edit"
 
 export default function useMode() {
-    // TODO: re-implement this hook so that it reads and writes the mode query parameter from the URL
-    return useReducer((mode: Mode) => (mode === "preview" ? "edit" : "preview"), "preview")
-}
+
+    const [searchParams, setSearchParams] = useSearchParams();
+
+    let mode = searchParams.get('mode')
+
+    // Als mode niet in de url zit zal de default preview zijn
+    if (!mode) {
+        mode = "preview"
+    }
+
+    // We maken een reducer dat switcht tussen edit en preview
+    const reducer = (state: any, action:any) => {
+        switch (action) {
+          case "setMode":
+            return state === "preview" ? "edit" : "preview";
+          default:
+            return state;
+        }
+    };
+
+    // We returnen de hudige mode en we zorgen ervoor dat de state geupdated kan worden
+    return [mode, () => {
+        const newMode = mode === "preview" ? "edit" : "preview";
+        setSearchParams({ mode: newMode });
+      }];
+    }
